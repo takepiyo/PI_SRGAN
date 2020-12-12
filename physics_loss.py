19595eb5d9f8f5901efcc8065ba0791edb10a0b9
takepiyo <exmcube@gmail.com>
2020-12-12 20:48:30 +0900

    physicsの損失関数の実装　動作未確認

---
+++ physics_loss.py
@@ -0,0 +1,109 @@
+import torch
+import torch.nn as nn
+
+
+class PhysicsInformedLoss(nn.Module):
+
+    def __init__(self, labmda_con, dx):
+        super(PhysicsInformedLoss, self).__init__()
+        self.labmda_con = labmda_con
+        self.dx = dx
+        self.first_order_diff_x = self.get_diff_filter(0, 1)
+        self.first_order_diff_y = self.get_diff_filter(1, 1)
+        self.second_order_diff_x = self.get_diff_filter(0, 2)
+        self.second_order_diff_y = self.get_diff_filter(1, 2)
+
+    def forward(self, gen_output, HR_image):
+        vel_grad = self.get_velocity_grad(gen_output)
+        vel_grad_HR = self.get_velocity_grad(HR_image)
+        strain_rate_2_HR = torch.mean(torch.mean(self.get_strain_rate_mags(
+            vel_grad_HR), axis=1, keepdim=True), axis=2, keepdim=True)
+
+        continuity_loss = self.get_continuity_loss(vel_grad)
+        pressure_loss = self.get_pressure_loss(gen_output, vel_grad)
+
+        continuity_loss = torch.square(continuity_loss) / strain_rate_2_HR
+        pressure_loss = torch.square(pressure_loss) / strain_rate_2_HR ** 2
+
+        return self.labmda_con * continuity_loss + (1 - self.labmda_con) * pressure_loss
+
+    def get_velocity_grad(self, input):
+        dudx = self.ddx(input, 0)
+        dvdx = self.ddx(input, 1)
+        dudy = self.ddy(input, 0)
+        dvdy = self.ddy(input, 1)
+        return dudx, dvdx, dudy, dvdy
+
+    def get_strain_rate_mags(self, vel_grad):
+        dudx, dvdx, dudy, dvdy = vel_grad
+
+        strain_rate_mag2 = dudx**2 + dvdy**2 + 2 * \
+            ((0.5 * (dudy + dvdx))**2)
+
+        return strain_rate_mag2
+
+    def get_continuity_loss(self, vel_grad):
+        dudx, dvdx, dudy, dvdy = vel_grad
+        return dudx + dvdy
+
+    def get_pressure_loss(self, input, vel_grad):
+        dudx, dvdx, dudy, dvdy = vel_grad
+        d2pdx2 = self.d2dx2(input, 2)
+        d2pdy2 = self.d2dy2(input, 2)
+
+        return d2pdx2 + d2pdy2 + dudx * dudx + dvdy * dvdy + 2 * (dudy * dvdx)
+
+    def ddx(self, input, channel):
+        return self.first_order_diff_x(input[:, channel, :, :]) / self.dx
+
+    def ddy(self, input, channel):
+        return self.first_order_diff_y(input[:, channel, :, :]) / self.dx
+
+    def d2dx2(self, input, channel):
+        return self.second_order_diff_x(input[:, channel, :, :]) / self.dx ** 2
+
+    def d2dy2(self, input, channel):
+        return self.second_order_diff_y(input[:, channel, :, :]) / self.dx ** 2
+
+    def get_diff_filter(self, direction, order):
+        '''
+        direction = 0 : x
+        direction = 1 : y
+        '''
+        filter = nn.Conv2d(1, 1, 7, 1, 3, bias=False)
+        if order == 1:
+            w_0 = 0.
+            w_1 = -3. / 4.
+            w_2 = 3. / 20.
+            w_3 = -1. / 60.
+        elif order == 2:
+            w_0 = -49. / 18.
+            w_1 = 3. / 2.
+            w_2 = -3. / 20.
+            w_3 = 1. / 90.
+        else:
+            print("order is accepted 1 or 2")
+            exit()
+        if direction == 0:
+            weight = torch.tensor([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                                   [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                                   [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                                   [w_3, w_2, w_1, w_0, w_1, w_2, w_3],
+                                   [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                                   [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                                   [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]], dtype=torch.float32)
+            filter.weight = weight
+        elif direction == 1:
+            weight = torch.tensor([[0.0, 0.0, 0.0, w_3, 0.0, 0.0, 0.0],
+                                   [0.0, 0.0, 0.0, w_2, 0.0, 0.0, 0.0],
+                                   [0.0, 0.0, 0.0, w_1, 0.0, 0.0, 0.0],
+                                   [0.0, 0.0, 0.0, w_0, 0.0, 0.0, 0.0],
+                                   [0.0, 0.0, 0.0, w_1, 0.0, 0.0, 0.0],
+                                   [0.0, 0.0, 0.0, w_2, 0.0, 0.0, 0.0],
+                                   [0.0, 0.0, 0.0, w_3, 0.0, 0.0, 0.0]], dtype=torch.float32)
+            filter.weight = weight
+        else:
+            print("direction is accepted 1 or 2")
+            exit()
+
+        return filter
